a0aef659d5381bce896748a1b85aa1b87965479c
ComeBackCity <50582070+ComeBackCity@users.noreply.github.com>
2021-02-27 17:35:05 +0600

    Added dicitonary insertion. Will add token for verification later

---
+++ src/functions/content_insertion/dictionary.js
@@ -0,0 +1,22 @@
+const dictionaryModel = require('../../models/dictionary')
+const status_codes = require('../../utils/status_code/status_codes')
+
+const insertIntoDictionary = async (req, res) => {
+    const word = await dictionaryModel.create(req.body)
+
+    if(word){
+        return res.status(status_codes.SUCCESS)
+            .send({
+                message: "Word inserted successfully"
+            })
+    }
+    else {
+        return res.status(status_codes.INTERNAL_SERVER_ERROR)
+            .send({
+                error: "Something went wrong"
+            })
+    }
+}
+
+module.exports = insertIntoDictionary
+
--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -5,6 +5,7 @@ const signOut = require('../functions/Authentication/Admin/signOut')
 const createAdmin = require('../functions/Authentication/Admin/createAdmin')
 const updateRole = require('../functions/Authentication/Admin/changeRole')
 const initialSuperAdmin = require('../functions/Authentication/Admin/initialCreate')
+const insertIntoDictionary = require('../functions/content_insertion/dictionary')
 
 //Authentication
 router.post('/admin/login',signIn)
@@ -12,5 +13,8 @@ router.delete('/admin/logout',signOut)
 router.post('/admin/create_admin',createAdmin)
 router.patch('/admin/update_role',updateRole)
 router.post('/admin/initial',initialSuperAdmin) //Somehow need to make it one time use
+
+//Dictionary and content
+router.post('/dictionary/insert',insertIntoDictionary)
 
 module.exports = router
