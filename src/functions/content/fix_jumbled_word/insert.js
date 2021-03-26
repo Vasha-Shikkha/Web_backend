28855405b1c196ede21f7d7a52f41315d5d7da34
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-26 19:35:30 +0600

    updated controllers

--- src/functions/content/fix_jumbled_word/insert.js
+++ src/functions/content/fix_jumbled_word/insert.js
@@ -16,6 +16,8 @@ const insertJumbledWord = async (req, res) => {
 		tasks.push({
 			topic_id: task.topic_id,
 			level: task.level_requirement,
+			instruction: task.instruction,
+			instructionImage: task.instructionImage,
 			name: "Jumbled Word",
 		});
 
