9594d91e3ee535dff857c9db4e057c4005f3ccb1
Waqar-107 <waqar.hassan866@gmail.com>
2021-04-28 23:58:05 +0600

    minor bug fixed

--- src/functions/content/caption_matching/insert.js
+++ src/functions/content/caption_matching/insert.js
@@ -5,98 +5,98 @@ const topicLevelCreator = require("../../../utils/database/topicLevelCreator");
 const status_codes = require("../../../utils/status_code/status_codes");
 
 const insertCaptionMatching = async (req, res) => {
-    req.setTimeout(10 * 1000);
+	req.setTimeout(10 * 1000);
 
-    let tasks = [],
-        taskIDs = [],
-        subTaskEntries = [],
-        subTaskIDs = [],
-        entries = [];
+	let tasks = [],
+		taskIDs = [],
+		subTaskEntries = [],
+		subTaskIDs = [],
+		entries = [];
 
-    for (let task of req.body) {
-        tasks.push({
-            topic_id: task.topic_id,
-            level: task.level_requirement,
-            instruction: task.instruction,
-            instructionImage: task.instructionImage,
-            name: "Sentence Matching",
-        });
+	for (let task of req.body) {
+		tasks.push({
+			topic_id: task.topic_id,
+			level: task.level_requirement,
+			instruction: task.instruction,
+			instructionImage: task.instructionImage,
+			name: "Drag Caption to Picture",
+		});
 
-        // check if that topic-level-count exists
-        // if not then create
-        // else update
-        await topicLevelCreator(task.topic_id, task.level_requirement);
-    }
+		// check if that topic-level-count exists
+		// if not then create
+		// else update
+		await topicLevelCreator(task.topic_id, task.level_requirement);
+	}
 
-    // insert the tasks in the task-table
-    await taskModel
-        .bulkCreate(tasks, {
-            fields: ["topic_id", "level", "name", "instruction", "instructionImage"],
-        })
-        .then((res) => {
-            for (let task of res) {
-                taskIDs.push(task.dataValues.id);
-            }
-        })
-        .catch((err) => {
-            return res.status(status_codes.INTERNAL_SERVER_ERROR).send({
-                error: "Something went Wrong",
-            });
-        });
+	// insert the tasks in the task-table
+	await taskModel
+		.bulkCreate(tasks, {
+			fields: ["topic_id", "level", "name", "instruction", "instructionImage"],
+		})
+		.then((res) => {
+			for (let task of res) {
+				taskIDs.push(task.dataValues.id);
+			}
+		})
+		.catch((err) => {
+			return res.status(status_codes.INTERNAL_SERVER_ERROR).send({
+				error: "Something went Wrong",
+			});
+		});
 
-    // add sub-tasks
-    for (let i in req.body) {
-        let task = req.body[i];
-        for (let j = 0; j < task.subtask.length; j++) {
-            subTaskEntries.push({
-                task_id: taskIDs[i],
-            });
-        }
-    }
+	// add sub-tasks
+	for (let i in req.body) {
+		let task = req.body[i];
+		for (let j = 0; j < task.subtask.length; j++) {
+			subTaskEntries.push({
+				task_id: taskIDs[i],
+			});
+		}
+	}
 
-    await subTaskModel
-        .bulkCreate(subTaskEntries, {
-            fields: ["task_id"],
-        })
-        .then((r) => {
-            for (let subTask of r) {
-                subTaskIDs.push(subTask.dataValues.id);
-            }
-        })
-        .catch((err) => {
-            return res.status(status_codes.INTERNAL_SERVER_ERROR).send({
-                error: "Something went wrong",
-            });
-        });
+	await subTaskModel
+		.bulkCreate(subTaskEntries, {
+			fields: ["task_id"],
+		})
+		.then((r) => {
+			for (let subTask of r) {
+				subTaskIDs.push(subTask.dataValues.id);
+			}
+		})
+		.catch((err) => {
+			return res.status(status_codes.INTERNAL_SERVER_ERROR).send({
+				error: "Something went wrong",
+			});
+		});
 
-    let k = 0;
+	let k = 0;
 
-    for (let i in req.body) {
-        let task = req.body[i];
-        for (let j = 0; j < task.subtask.length; j++) {
-            let subTask = task.subtask[j];
-            subTask.subTask_id = subTaskIDs[k];
-            k++;
-            entries.push(subTask);
-        }
-    }
+	for (let i in req.body) {
+		let task = req.body[i];
+		for (let j = 0; j < task.subtask.length; j++) {
+			let subTask = task.subtask[j];
+			subTask.subTask_id = subTaskIDs[k];
+			k++;
+			entries.push(subTask);
+		}
+	}
 
-    await captionMatchingModel
-        .bulkCreate(entries, {
-            fields: ["subTask_id", "image", "caption", "explanation"],
-        })
-        .then((r) => {
-            if (r !== undefined)
-                return res.status(status_codes.SUCCESS).send({
-                    message: "Content insertion successful",
-                });
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(status_codes.INTERNAL_SERVER_ERROR).send({
-                error: "Something went wrong",
-            });
-        });
+	await captionMatchingModel
+		.bulkCreate(entries, {
+			fields: ["subTask_id", "image", "caption", "explanation"],
+		})
+		.then((r) => {
+			if (r !== undefined)
+				return res.status(status_codes.SUCCESS).send({
+					message: "Content insertion successful",
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+			return res.status(status_codes.INTERNAL_SERVER_ERROR).send({
+				error: "Something went wrong",
+			});
+		});
 };
 
 module.exports = insertCaptionMatching;
