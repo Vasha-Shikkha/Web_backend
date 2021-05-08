1375912006772b83cc48255e31bec5407ba5839b
Waqar-107 <waqar.hassan866@gmail.com>
2021-05-08 20:35:26 +0600

    word to picture done according to new data flow

--- src/functions/tasks/TaskFactory.js
+++ src/functions/tasks/TaskFactory.js
@@ -1,34 +1,51 @@
 const FetchWordToPicture = require("./word_to_picture/wordToPicture");
 const subTaskModel = require("../../models/sub_task");
+const {Op} = require("sequelize");
 
 const TaskFactory = async (tasks) => {
-	let data = [];
 	let taskId = [];
 	let subTaskId = [];
+	let taskMapping = new Map();
+	let subTaskAndTaskMapping = new Map();
 
+	let idx = 0;
 	for (let task of tasks) {
 		taskId.push(task.dataValues.id);
+		taskMapping.set(task.dataValues.id, idx++);
 	}
 
 	const subTasks = await subTaskModel.findAll({
 		where: {
 			task_id: {
-				[Op.in]: allTasks,
+				[Op.in]: taskId,
 			},
 		},
 	});
 
+	idx = 0;
 	for (let subTask of subTasks) {
 		subTaskId.push(subTask.dataValues.id);
+		subTaskAndTaskMapping.set(subTask.dataValues.id, subTask.dataValues.task_id);
 	}
-	// switch (task.name) {
-	// 	case "Word to Picture":
-	// 		return await FetchWordToPicture(task);
-	// 	default:
-	// 		return {msg: "nai"};
-	// }
-	console.log(tasks);
-	return [];
+
+	let questions = [];
+	for (let task of tasks) questions.push({taskDetail: task.dataValues});
+
+	for (let task of tasks) {
+		let data = {};
+		switch (task.name) {
+			case "Word to Picture":
+				data = await FetchWordToPicture(subTaskId);
+				if (!data.error) {
+					idx = taskMapping.get(subTaskAndTaskMapping.get(data.question[0].subTaskId));
+					questions[idx].question = data.question;
+				}
+			default:
+				break;
+		}
+	}
+
+	return questions;
 };
 
 module.exports = TaskFactory;
--- src/functions/tasks/word_to_picture/wordToPicture.js
+++ src/functions/tasks/word_to_picture/wordToPicture.js
@@ -1,31 +1,31 @@
 const WordToPictureModel = require("../../../models/word_picture");
+const {Op} = require("sequelize");
 
 const FetchWordToPicture = async (subTaskId) => {
-	await WordToPictureModel.findAll({
+	let questions = await WordToPictureModel.findAll({
 		where: {
 			subTask_id: {
 				[Op.in]: subTaskId,
 			},
 		},
-	})
-		.then((questions) => {
-			let returnableQuestion = [];
-			for (let question of questions) {
-				let exercise = {
-					subTaskId: question.dataValues.subTask_id,
-					question: question.dataValues.question,
-					images: question.dataValues.images,
-					answer: question.dataValues.answer,
-					explanation: question.dataValues.explanation,
-				};
+	});
 
-				returnableQuestion.push(exercise);
-				return {error: null, question: returnableQuestion};
-			}
-		})
-		.catch((error) => {
-			return {error, question: []};
-		});
+	if (!questions) return {error, question: []};
+
+	let returnableQuestion = [];
+	for (let question of questions) {
+		let exercise = {
+			subTaskId: question.dataValues.subTask_id,
+			question: question.dataValues.question,
+			images: question.dataValues.images,
+			answer: question.dataValues.answer,
+			explanation: question.dataValues.explanation,
+		};
+
+		returnableQuestion.push(exercise);
+	}
+
+	return {error: null, question: returnableQuestion};
 };
 
 module.exports = FetchWordToPicture;
