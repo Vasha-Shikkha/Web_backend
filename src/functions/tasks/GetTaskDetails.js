247bf9188806dd4ca4b243113ea81465860c781b
ComeBackCity <50582070+ComeBackCity@users.noreply.github.com>
2021-08-06 19:59:25 +0600

    Updated get task details query

--- src/functions/tasks/GetTaskDetails.js
+++ src/functions/tasks/GetTaskDetails.js
@@ -1,8 +1,32 @@
 const taskModel = require("../../models/task");
+const topicModel = require('../../models/topic');
 const status = require("../../utils/status_code/status_codes");
 
 const getTaskDetails = async (req, res) => {
 	const tasks = await taskModel.findAll();
+
+	/*
+	*TODO: fetching topic name for each task has been done in a naive approach.
+	* It can be improved my creating association between topic and task.
+	 */
+
+	let topics = {};
+
+	await topicModel.findAll()
+		.then(r => {
+			r.forEach(topic => {
+				topics[topic.id] = topic.name
+			})
+		})
+		.catch(() => {
+			return res.status(status.INTERNAL_SERVER_ERROR).json({
+				error: "Error while fetching topics",
+			});
+		})
+
+	tasks.forEach(task => {
+		task.dataValues.topic_name = topics[task.topic_id]
+	})
 
 	try {
 		return res.status(status.SUCCESS).send(tasks);
