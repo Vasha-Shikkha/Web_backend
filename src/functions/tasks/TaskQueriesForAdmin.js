6ec1cce292040180ed0fcb8791d1b04f134e69c8
Waqar-107 <waqar.hassan866@gmail.com>
2021-10-02 19:54:49 +0600

    updated the query

--- src/functions/tasks/TaskQueriesForAdmin.js
+++ src/functions/tasks/TaskQueriesForAdmin.js
@@ -1,12 +1,23 @@
 const taskModel = require("../../models/task");
+const {QueryTypes} = require("sequelize");
+const sequelize = require("../../utils/database/database");
 const statusCodes = require("../../utils/status_code/status_codes");
 
 exports.GetAllTasksByName = async (req, res) => {
 	const taskName = req.query.taskName;
-	try {
-		const tasks = await taskModel.findAll({where: {name: taskName}});
-		res.status(statusCodes.SUCCESS).json(tasks);
-	} catch (e) {
-		res.status(statusCodes.INTERNAL_SERVER_ERROR).send("Something Went Wrong");
-	}
+	const sql = `select t.id, t.level, tp.name, tp.type
+		 from "Task" as t
+		 inner join "Topic" as tp on t.topic_id = tp.id
+		 where t.name = '${taskName}'`;
+
+	await sequelize
+		.query(sql, {
+			type: QueryTypes.SELECT,
+		})
+		.then(async (value) => {
+			res.status(statusCodes.SUCCESS).json(value);
+		})
+		.catch((err) => {
+			res.status(statusCodes.INTERNAL_SERVER_ERROR).send("Something Went Wrong");
+		});
 };
